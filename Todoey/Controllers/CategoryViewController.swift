48fb3fd8949fe741e9b518a123650fa5f72b41c2
Omar Ashraf <amourakora2001@gmail.com>
2023-10-31 19:57:26 +0200

    add empty tableviewcontroller to show different categories for todo items

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Omar Ashraf on 31/10/2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
